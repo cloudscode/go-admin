da1448c03eb7303b31bb5003b1f91f36d08e86e1
ljzhuanjiao@gmail.com <ljzhuanjiao@gmail.com>
2021-05-28 09:25:37 +0800

    优化数据权限

--- common/actions/permission.go
+++ common/actions/permission.go
@@ -96,9 +96,9 @@ func PermissionUser(tableName string, p *DataPermission) func(db *gorm.DB) *gorm
 		case "2":
 			return db.Where(tableName+".user_id in (select sys_user.user_id from sys_role_dept left join sys_user on sys_user.dept_id=sys_role_dept.dept_id where sys_role_dept.role_id = ?)", p.RoleId)
 		case "3":
-			return db.Where(tableName+".create_by in (SELECT user_id from sys_user where dept_id = ? )", p.DeptId)
+			return db.Where(tableName+".user_id in (SELECT user_id from sys_user where dept_id = ? )", p.DeptId)
 		case "4":
-			return db.Where(tableName+".create_by in (SELECT user_id from sys_user where sys_user.dept_id in(select dept_id from sys_dept where dept_path like ? ))", "%/"+pkg.IntToString(p.DeptId)+"/%")
+			return db.Where(tableName+".user_id in (SELECT user_id from sys_user where sys_user.dept_id in(select dept_id from sys_dept where dept_path like ? ))", "%/"+pkg.IntToString(p.DeptId)+"/%")
 		//case "5":
 		//	return db.Where(tableName+".create_by = ?", p.UserId)
 		default             :
